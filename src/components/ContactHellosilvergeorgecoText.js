f056e8d206525b346ef187790553ce91dd6099c1
lilbobb <lilbobb>
2023-10-07 09:09:29 +0000

    First Commit from Locofy Builder

---
+++ src/components/ContactHellosilvergeorgecoText.js
@@ -0,0 +1,11 @@
+import "./ContactHellosilvergeorgecoText.css";
+
+const ContactHellosilvergeorgecoText = () => {
+  return (
+    <b className="contact-hellosilvergeorgeco">
+      Contact: Hello@silvergeorge.com
+    </b>
+  );
+};
+
+export default ContactHellosilvergeorgecoText;
---
+++ src/components/Mospending.js
@@ -0,0 +1,17 @@
+import "./Mospending.css";
+
+const Mospending = () => {
+  return (
+    <div className="mospending">
+      <div className="frame">
+        <img className="mospending-1-1" alt="" src="/mospending-1-1@2x.png" />
+      </div>
+      <div className="frame1">
+        <img className="mospending-1-2" alt="" src="/mospending-1-2@2x.png" />
+        <img className="noise-icon" alt="" src="/noise@2x.png" />
+      </div>
+    </div>
+  );
+};
+
+export default Mospending;
